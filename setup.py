3208361163e1f60152c152f800df07795d128bb1
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2023-07-19 19:39:11 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='simple_gridworld',
+      version='0.01',
+      description='Simple Gridworld - Used to learn how to create an environment',
+      url='https://github.com/jkcooper2/gym_bandits',
+      author='Ahad Jawaid',
+      packages=['simple_gridworld'],
+      author_email='',
+      license='MIT License',
+      install_requires=['gym>=0.2.3'],
+)
