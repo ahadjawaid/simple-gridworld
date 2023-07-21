d64494a4edeb5a4dee0bd2aa58c2bed4d8668ff9
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2023-07-20 17:13:51 -0700

    docs: change the github url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='simple_gridworld',
       version='0.01',
       description='Simple Gridworld - Used to learn how to create an environment',
-      url='https://github.com/jkcooper2/gym_bandits',
+      url='https://github.com/ahadjawaid/simple-gridworld',
       author='Ahad Jawaid',
       packages=['simple_gridworld'],
       author_email='',
